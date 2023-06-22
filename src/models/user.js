0f04ddadabd0468bc8aa0606a2737e392f1e850a
rubenesteban1291 <rube.vasquez@duocuc.cl>
2023-06-22 17:01:12 -0600

    arquetipo test

---
+++ src/models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    edad: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
