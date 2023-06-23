56238ad0a220d9c992fb3dae21fb1e3289dcdd19
rvasquez1291 <121567208+rvasquez1291@users.noreply.github.com>
2023-06-22 20:55:37 -0600

    Update user.js
    deleteOne

--- src/routes/user.js
+++ src/routes/user.js
@@ -43,7 +43,7 @@ router.put("/users/:id", (req, res) => {
 router.delete("/users/:id", (req, res) => {
     const { id } = req.params;
     userSchema
-        .remove({ _id: id })
+        .deleteOne({ _id: id })
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
